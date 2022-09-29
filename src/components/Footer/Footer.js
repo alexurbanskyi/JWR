df260693ad05c62bc54d1beac3eec194344b842e
kalitsinskyi <vlad.kalitsinskij@transfotech.com>
2022-09-29 18:05:48 +0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import './Footer.css'
+import { FaPhoneAlt } from "react-icons/fa";
 
 function Footer() {
-  return (
-    <div className='footer'>Footer 2022</div>
+   return (
+      <div className='footer'>
+         <div className='footer-logo'>JWR</div>
+         <div className='worker-container'>
+            <div className='woker-info'>
+               <p className='woker-name'>Поліщук Віталій</p>
+               <div className='worker-link'> 
+                  <div className='link-icon'>
+                     <FaPhoneAlt/>
+                  </div>
+                  <div className='link-info'>
+                   <a className='tel' href="tel: +380967774389">+38(096)7774389</a>
+                  </div>
+               </div>              
+            </div>
+            <div className='woker-info'>
+               <p className='woker-name'>Ромасюнь Віталій</p>
+               <div className='worker-link'> 
+                  <div className='link-icon'>
+                     <FaPhoneAlt/>
+                  </div>
+                  <div className='link-info'>
+                   <a className='tel' href="tel: +380967774389">+38(096)7774389</a>
+                  </div>
+               </div>              
+            </div>
+         </div>
+         
+      </div>
     
-  )
+   )
 }
 export default Footer;
